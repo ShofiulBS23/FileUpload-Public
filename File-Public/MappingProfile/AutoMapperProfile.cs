3d6f2161163c8d36f26c47797d170a07c18791e7
Md. Shofiul Islam <shofiul.islam@brainstation-23.com>
2024-01-24 14:38:12 +0600

    Auto mapper configured

---
+++ File-Public/MappingProfile/AutoMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using File_Public.DbModels;
+using File_Public.Models;
+
+namespace File_Public.MappingProfile
+{
+    public class AutoMapperProfile : Profile
+    {
+       public AutoMapperProfile() {
+            CreateMap<Document,VmDocument>();
+            CreateMap<VmDocument, Document>();
+       }
+    }
+}
