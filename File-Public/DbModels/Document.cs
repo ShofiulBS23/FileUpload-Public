82b4437fdf5ddd0b363ce6d770df93e662268e0c
Md. Shofiul Islam <shofiul.islam@brainstation-23.com>
2024-01-24 14:21:19 +0600

    Document table created

---
+++ File-Public/DbModels/Document.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace File_Public.DbModels
+{
+    public class Document
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public Guid ClientId { get; set; }
+
+        [MaxLength(20)]
+        [Required]
+        public string ISIN { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Language { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string DocType { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public DateTime DocDate { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string DocName { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string DocExt { get; set; }
+
+    }
+}
--- File-Public/Program.cs
+++ File-Public/Program.cs
@@ -1,7 +1,13 @@
+using File_Public.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(
+        options => options.UseSqlServer(builder.Configuration.GetConnectionString("DbString"))
+    );
 
 var app = builder.Build();
 
