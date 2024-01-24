131e64962ec4459466596ddfeabff586f4ffcb29
Md. Shofiul Islam <shofiul.islam@brainstation-23.com>
2024-01-24 14:21:19 +0600

    Document table created

---
+++ File-Public/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using File_Public.DbModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace File_Public.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Document> Documents { get; set; }
+    }
+}
