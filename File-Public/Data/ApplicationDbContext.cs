fdaa2963c60957ba1afb1228c614367480eeee26
Md. Shofiul Islam <shofiul.islam@brainstation-23.com>
2024-01-24 15:50:19 +0600

    DocType table created

--- File-Public/Data/ApplicationDbContext.cs
+++ File-Public/Data/ApplicationDbContext.cs
@@ -10,5 +10,6 @@ namespace File_Public.Data
             
         }
         public DbSet<Document> Documents { get; set; }
+        public DbSet<DocTypeLU> DocTypes { get; set; }
     }
 }
---
+++ File-Public/Models/VmDocTypeLU.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace File_Public.Models
+{
+    public class VmDocTypeLU
+    {
+        public int DocTypeID { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string DocType { get; set; }
+    }
+}
